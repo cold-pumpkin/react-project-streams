cf1c1b4f3eb127005033f182715bfecb6cf5fda8
Philip <pch4623@gmail.com>
2022-02-12 12:22:30 +0900

    react-router-dom 활용하여 라우팅 준비

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Routes>
+              <Route path="/" exact element={<StreamList />} />
+              <Route path="/streams/new" exact element={<StreamCreate />} />
+              <Route path="/streams/edit" exact element={<StreamEdit />} />
+              <Route path="/streams/delete" exact element={<StreamDelete />} />
+              <Route path="/streams/show" exact element={<StreamShow />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
