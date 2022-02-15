cd4c09249e0ea486d83d28890732e2d67b916feb
Philip <pch4623@gmail.com>
2022-02-15 22:13:13 +0900

    Edit 버튼 클릭 시 해당 스트리밍 제목 노출

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { fetchStream } from '../../actions';
+import { connect } from 'react-redux';
 
-const StreamEdit = () => {
-  return <div>StreamEdit</div>;
-}
+const StreamEdit = ({ streams }) => {  // props에서 destructuring
+  const { id } = useParams();
+  
+  useEffect(() => {
+    fetchStream(id);
+  }, [id]);
+  
+  const stream = streams[id];
+  if (!stream)
+    return <div>Loading...</div>
+  
+  return (
+    <div>{stream.title}</div>
+  );
+};
 
-export default StreamEdit;
+const mapStateToProps = (state) => {
+  return { streams: state.streams }; // 위에서 props 로 사용 가능
+};
+
+export default connect(mapStateToProps, {fetchStream})(StreamEdit);
